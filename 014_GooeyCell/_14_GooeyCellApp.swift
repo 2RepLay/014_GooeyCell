489f4b7d31983475371a36a7d55e456efb5edc4c
Nikita Yankov <evne.yankovnikita@gmail.com>
2022-10-30 20:44:07 +0200

    Initial Commit

---
+++ 014_GooeyCell/_14_GooeyCellApp.swift
@@ -0,0 +1,17 @@
+//
+//  _14_GooeyCellApp.swift
+//  014_GooeyCell
+//
+//  Created by nikita on 30.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct _14_GooeyCellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
